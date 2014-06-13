d31ee33dee0b3488b434e895b5666f908d95cf5b
FullArray <sifreed77@gmail.com>
2014-06-12 22:48:39 -0400

    Initial General Sales tool

---
+++ SalesTool/src/com/apps/worldOfIce/SalesData.java
@@ -0,0 +1,11 @@
+package com.apps.worldOfIce;
+
+public class SalesData {
+	int data[] = {0,4,42};
+	
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("TODO: implement data display");
+	}
+
+}
