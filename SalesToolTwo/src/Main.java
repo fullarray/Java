68d20f183ad376f25c6e6596cbe8b2be2bbb3116
FullArray <sifreed77@gmail.com>
2014-06-12 23:50:37 -0400

    Hello thereme, FullArray is back again.

---
+++ SalesToolTwo/src/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("This APP SHOWS SALES DATA");
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		System.out.println("Test 4");
+	}
+
+}
---
+++ SalesToolTwo/src/SalesData.java
@@ -0,0 +1,11 @@
+
+
+public class SalesData {
+	int data[] = {0,4,42};
+	
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("TODO: implement data display");
+	}
+
+}
