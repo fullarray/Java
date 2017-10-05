5a18d5555d47805c7d0014b211cb5642ba232670
Jonathan <sifreed77@gmail.com>
2017-10-04 23:25:30 -0400

    Added another Struts implementation app without db

---
+++ app3/src/app2/LoginAction.java
@@ -0,0 +1,28 @@
+package app2;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import app2.LoginForm;
+
+public class LoginAction extends Action {
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		LoginForm loginForm = (LoginForm) form;
+
+		if (loginForm.getUserName() == null || loginForm.getPassword() == null
+				|| !loginForm.getUserName().equalsIgnoreCase("Mark")
+				|| !loginForm.getPassword().equals("Pa$$w0rd")) {
+			return mapping.findForward("success");
+		} else
+			return mapping.findForward("failure");
+	}
+
+}
