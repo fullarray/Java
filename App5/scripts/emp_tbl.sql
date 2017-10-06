0e8655cff193845cdce2b4b8b5704e96b0f3ce1b
Jonathan <sifreed77@gmail.com>
2017-10-06 15:49:38 -0400

    Added a simplified employee payroll application

---
+++ App5/scripts/emp_tbl.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS springEmployees2;
+
+use springEmployees2;
+
+CREATE TABLE `EMP_TBL` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `telephone` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
---
+++ App5/src/main/java/com/herrj/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.herrj.dao;
+
+import java.util.List;
+import com.herrj.model.Employee;
+
+public interface EmployeeDAO {
+
+	public void addEmployee(Employee employee);
+
+	public List<Employee> getAllEmployees();
+
+	public void deleteEmployee(Integer employeeId);
+
+	public Employee updateEmployee(Employee employee);
+
+	public Employee getEmployee(int employeeid);
+}
