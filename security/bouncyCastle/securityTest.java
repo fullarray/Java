3b8ea1c6d5a53fcfb7bc424f724db2ad5695a63d
Jonathan <sifreed77@gmail.com>
2017-12-04 23:06:21 -0500

    Added test classes for security libraries

---
+++ security/bouncyCastle/securityTest.java
@@ -0,0 +1,13 @@
+import java.security.Security;
+
+public class securityTest{
+    public static void main(String[] args) {
+        //Tell java to check if BouncyCastle is installed.
+        if (Security.getProvider("BC") == null){
+            System.out.println("Bouncy Castle is NOT installed");
+        }
+        else{
+            System.out.println("Bouncy Castle is installed");
+        }
+    }
+}
