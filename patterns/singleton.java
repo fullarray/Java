06d5bb25123e5f354381ce13395ca8017306387a
Jonathan <sifreed77@gmail.com>
2018-01-04 23:47:04 -0500

    Added singleton pattern implementation

---
+++ patterns/singleton.java
@@ -0,0 +1,20 @@
+//A single message using the singleton pattern
+public class SingleThing{
+	private static SingleThing instance = new SingleThing();
+	
+	private SingleThing(){}
+	
+	public static SingleThing getInstance(){
+		return instance;
+	}
+	
+	public void showMessage(){
+		System.out.printlin("Hey there!");
+	}
+}
+
+public class singleton{
+	SingleThing obj = SingleThing.getInstance();
+	
+	obj.showMessage();
+}
