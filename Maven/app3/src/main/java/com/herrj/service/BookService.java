2b87e250453b6212c4b6560a8410f0a23205328c
Jonathan <sifreed77@gmail.com>
2018-02-15 23:24:51 -0500

    Added spring/restful implemenntation

---
+++ Maven/app3/src/main/java/com/herrj/service/BookService.java
@@ -0,0 +1,13 @@
+package com.herrj.service;
+
+import java.util.List;
+import com.herrj.model.Book;
+
+public interface BookService {
+
+   long save(Book book);
+   Book get(long id);
+   List<Book> list();
+   void update(long id, Book book);
+   void delete(long id);
+}
