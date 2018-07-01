a14332733eeae3a2fbb897c467eb49a776da12f4
Jonathan <sifreed77@gmail.com>
2018-06-30 22:39:10 -0400

    Update EmployeeController.java

--- frameworks/spring/app6/src/main/java/com/journaldev/spring/controller/EmployeeController.java
+++ frameworks/spring/app6/src/main/java/com/journaldev/spring/controller/EmployeeController.java
@@ -28,13 +28,12 @@ public class EmployeeController {
 	
 	//Map to store employees, ideally we should use database
 	Map<Integer, Employee> empData = new HashMap<Integer, Employee>();
-	
 	@RequestMapping(value = EmpRestURIConstants.DUMMY_EMP, method = RequestMethod.GET)
 	public @ResponseBody Employee getDummyEmployee() {
 		logger.info("Start getDummyEmployee");
 		Employee emp = new Employee();
 		emp.setId(9999);
-		emp.setName("Dummy");
+		emp.setName("John Doe");
 		emp.setCreatedDate(new Date());
 		empData.put(9999, emp);
 		return emp;
