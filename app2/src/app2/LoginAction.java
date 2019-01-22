cf8fc8252ac847c24afcf62d79791b38f783170c
Jonathan <sifreed77@gmail.com>
2019-01-21 23:07:59 -0500

    Update LoginAction.java

--- app2/src/app2/LoginAction.java
+++ app2/src/app2/LoginAction.java
@@ -2,7 +2,6 @@ package app2;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
