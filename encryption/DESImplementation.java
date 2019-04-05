ef3f1ce7126c360c57fd74ac7047fa32aa6dc09b
Jonathan <sifreed77@gmail.com>
2019-04-04 22:51:13 -0400

    Update DESImplementation.java

--- encryption/DESImplementation.java
+++ encryption/DESImplementation.java
@@ -1,5 +1,5 @@
 public class DESImplementation {
-	/* Initial Permutation */
+	/* Initial Permutations */
 	static final int[] IP = {
 		58, 50, 42, 34, 26, 18, 10, 2,
 		60, 52, 44, 36, 28, 20, 12, 4,
