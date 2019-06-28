82e011977d0ebc5addf2ef76093393ee035dec33
Jonathan <sifreed77@gmail.com>
2019-06-27 22:55:34 -0400

    Update SalesData.java

--- SalesTool/src/com/apps/worldOfIce/SalesData.java
+++ SalesTool/src/com/apps/worldOfIce/SalesData.java
@@ -3,7 +3,7 @@ package com.apps.worldOfIce;
 public class SalesData {
 	int data[] = {0,4,22};
 	public void display() {
-		// TODO Auto-generated method stubs
+		// TODO Auto-generated method stubse
 		System.out.println("TODO");
 	}
 
