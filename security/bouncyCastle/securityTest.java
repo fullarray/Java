421f4090ef0a28cc96199f141087a4561e9fdda0
Jonathan <sifreed77@gmail.com>
2019-07-08 23:53:22 -0400

    Update securityTest.java

--- security/bouncyCastle/securityTest.java
+++ security/bouncyCastle/securityTest.java
@@ -2,7 +2,7 @@ import java.security.Security;
 
 public class securityTest{
     public static void main(String[] args) {
-        //Tell java to check if BouncyCastle is installed.
+        //Tell java to check if BouncyCastl is installed.
         if (Security.getProvider("BC") == null){
             System.out.println("Bouncy Castle is NOT installed");
         }
