ad9671485e44d2affc398dbaae9ea10f88c55344
Jonathan <sifreed77@gmail.com>
2019-07-13 23:43:18 -0400

    Update emp_tbl.sql

--- App5/scripts/emp_tbl.sql
+++ App5/scripts/emp_tbl.sql
@@ -3,7 +3,7 @@ use springEmployees2;
 CREATE TABLE `EMP_TBL` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `email` varchar(40) NOT NULL,
   `address` varchar(45) NOT NULL,
   `telephone` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
