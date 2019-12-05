28c2228a94cd5c851331212c3a4783ef646553de
Jonathan <sifreed77@gmail.com>
2019-12-04 23:30:57 -0500

    Update LoginForm.java

--- app3/src/app2/LoginForm.java
+++ app3/src/app2/LoginForm.java
@@ -2,7 +2,6 @@ package app2;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
-
 public class LoginForm extends ActionForm {
 	private String userName = null;
 	private String password = null;
