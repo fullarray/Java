7f4c6035c5344e676c83f5d634856d765d39b6e5
Jonathan <sifreed77@gmail.com>
2019-12-26 23:23:48 -0500

    Update emp_tbl.sql

--- app4/scripts/emp_tbl.sql
+++ app4/scripts/emp_tbl.sql
@@ -10,3 +10,13 @@ CREATE TABLE `EMP_TBL` (
   `telephone` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `EMP_TBL` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `telephone` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
