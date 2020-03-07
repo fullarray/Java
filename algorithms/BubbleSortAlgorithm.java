d127a28b62fc6ffbd996a8e98079fce62e3e93c0
Jonathan <sifreed77@gmail.com>
2020-03-06 23:22:27 -0500

    Update BubbleSortAlgorithm.java

--- algorithms/BubbleSortAlgorithm.java
+++ algorithms/BubbleSortAlgorithm.java
@@ -1,8 +1,8 @@
 public class BubbleSortAlgorithm
-{public static void main(String[] args)
+{
+	public static void main(String[] args)
 	{
-		/*
-		 * First begin by declaring an unsorted array.
+		/* First begin by declaring an unsorted array.
 		 * This array will be used as a test subject for this
          	 * small implementation. This new array below runs a new ex
 		 */
