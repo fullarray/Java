e1e9fcf2e32fd2c72356aacb583210a5cbf9266b
Jonathan <sifreed77@gmail.com>
2020-06-26 23:52:28 -0400

    Update LoginForm.java

--- app2/src/app2/LoginForm.java
+++ app2/src/app2/LoginForm.java
@@ -9,15 +9,19 @@ public class LoginForm extends ActionForm {
 	public String getUserName() {
 		return userName;
 	}
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	@Override
 	public void reset(ActionMapping mapping, HttpServletRequest request) {
 		this.password = null;
