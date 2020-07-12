5cbdef549b5934cb5da496a7312364d4937f6a36
Jonathan <sifreed77@gmail.com>
2020-07-11 23:36:54 -0400

    Update Example.java

--- frameworks/spring/app7/src/main/java/Example.java
+++ frameworks/spring/app7/src/main/java/Example.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class Example{
    @RequestMapping("/")
    String home(){
-     return "Hello World!";
+     return "Hello Worlds!";
    }
 
    public static void main(String[] args) throws Exception{
