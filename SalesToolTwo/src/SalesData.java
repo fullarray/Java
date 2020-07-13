ab0cf054819b668d45949418d28e0b090da9b954
Jonathan <sifreed77@gmail.com>
2020-07-12 22:43:04 -0400

    Update SalesData.java

--- SalesToolTwo/src/SalesData.java
+++ SalesToolTwo/src/SalesData.java
@@ -3,7 +3,7 @@ public class SalesData {
 	int data[] = {0,4,42};
 	public void display() {
 		// TODO Auto-generate methods
-		System.out.println("TODO: implement data displays");
+		System.out.println("TODO: implement data disp");
 	}
 
 }
