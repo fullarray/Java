77b9dd1f85b06f7bbfb212cdc97c370ecbfa2118
Jonathan <sifreed77@gmail.com>
2020-08-19 23:45:17 -0400

    Update hashTable.java

--- handCode/hashTable.java
+++ handCode/hashTable.java
@@ -5,9 +5,7 @@ public class hashTable{
 	public static void main(String[] argv){
 		Enumeration names;
 		String key;
-		
 		Hashtable<Integer, String> hashtable = new Hashtable<Integer, String>();
-		
 		hashtable.put("001","John");
 		hashtable.put("002","James");
 		hashtable.put("003","Josh");
