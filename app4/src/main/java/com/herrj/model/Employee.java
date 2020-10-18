574f68259b2368c0e852d3f91e14842439b8fd44
Jonathan <sifreed77@gmail.com>
2020-10-17 22:44:04 -0400

    Update Employee.java

--- app4/src/main/java/com/herrj/model/Employee.java
+++ app4/src/main/java/com/herrj/model/Employee.java
@@ -32,11 +32,9 @@ public class Employee implements Serializable {
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
