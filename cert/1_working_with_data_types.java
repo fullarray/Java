9659f4edaf91dfdb82c8197307b324955c6d124c
Jonathan <sifreed77@gmail.com>
2020-12-31 22:39:49 -0500

    Update 1_working_with_data_types.java

--- cert/1_working_with_data_types.java
+++ cert/1_working_with_data_types.java
@@ -2,8 +2,8 @@
 public 1_use_primitives_and_wrapper_classes{
 	public static main void(String [] args){
 		Integer num = 5;
-		Double price = 1.99;
-		Character initials = 'AJss';
+		Double price = 1.59;
+		Character initials = 'AJs';
 		System.out.println(num);
 		System.out.println(price);
 		System.out.println(initials);
