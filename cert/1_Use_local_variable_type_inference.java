1b03ed5b042640cf704c11a0b552fea386b41a2a
Jonathan <sifreed77@gmail.com>
2021-01-16 23:25:50 -0500

    Update 1_Use_local_variable_type_inference.java

--- cert/1_Use_local_variable_type_inference.java
+++ cert/1_Use_local_variable_type_inference.java
@@ -3,7 +3,7 @@ import java.*;
 import java.util.*;
 public 1_Use_local_variable_type_inference{
 	public static main void(String [] args){
-		//
+		
 		// before java 10s
 		Path path = Paths.get("src/web.log");
 		try(Stream<String> lines = Files.lines(path)){
