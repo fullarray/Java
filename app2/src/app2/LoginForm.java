224a1a78fc8ba8e7e619dd1b1f8bbfd3338fbeac
Jonathan <sifreed77@gmail.com>
2021-03-13 22:36:59 -0500

    Update LoginForm.java

--- app2/src/app2/LoginForm.java
+++ app2/src/app2/LoginForm.java
@@ -9,11 +9,9 @@ public class LoginForm extends ActionForm {
 	public String getUserName() {
 		return userName;
 	}
-	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
 	public String getPassword() {
 		return password;
 	}
