5fe770c91a8e9f3023d250f2ff68939329b0e7d6
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-06-11 21:44:45 -0400

    Update emp_tbl.sql

--- App5/scripts/emp_tbl.sql
+++ App5/scripts/emp_tbl.sql
@@ -3,10 +3,15 @@ CREATE DATABASE IF NOT EXISTS springEmployees2;
 use springEmployees2;
 
 CREATE TABLE `EMP_TBL` (
+
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  
   `name` varchar(45) NOT NULL,
+  
   `email` varchar(40) NOT NULL,
+  
   `address` varchar(45) NOT NULL,
+  
   `telephone` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
