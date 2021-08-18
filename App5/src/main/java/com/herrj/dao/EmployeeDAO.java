e1d5f3a4ea2e04a094422689cf27073973233a26
Jonathan <sifreed77@gmail.com>
2021-08-17 22:51:52 -0400

    Update EmployeeDAO.java

--- App5/src/main/java/com/herrj/dao/EmployeeDAO.java
+++ App5/src/main/java/com/herrj/dao/EmployeeDAO.java
@@ -6,12 +6,8 @@ import com.herrj.model.Employee;
 public interface EmployeeDAO {
 
 	public void addEmployee(Employee employee);
-
 	public List<Employee> getAllEmployees();
-
 	public void deleteEmployee(Integer employeeId);
-
 	public Employee updateEmployee(Employee employee);
-
 	public Employee getEmployee(int employeeid);
 }
