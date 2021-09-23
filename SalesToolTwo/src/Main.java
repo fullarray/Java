cd95b0ac7569d37be817d4360a476f0955da3815
Jonathan <sifreed77@gmail.com>
2021-09-22 22:04:24 -0400

    Update Main.java

--- SalesToolTwo/src/Main.java
+++ SalesToolTwo/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 	public static void main(String[] args) {
 		SalesData data = new SalesData();
 		displayGreeting();
+		
 		data.display();
 	}
 	private static void displayGreeting(){
